61c84711f8f5b9d06af8b704487efbc4512ed50d
Hannah Baker <hannahb@cogapp.com>
2024-05-17 12:17:21 +0100

    Integrate cogapp ai images

--- app/Openseadragon.js
+++ app/Openseadragon.js
@@ -9,16 +9,34 @@ export function OpenSeadragonViewer({artworkId, idPrefix}) {
 
     useEffect(() => {
         // Construct the manifest url 
-        let manifestUrl = 'https://api.artic.edu/api/v1/artworks/' + artworkId + '/manifest.json';
+        let manifestUrl;
+        let cogappImage = /[a-zA-Z]/.test(artworkId);
+
+        if (cogappImage) {
+            manifestUrl = 'https://storiiies-images.cogapp.com/iiif/2/' + artworkId + '.ptif/info.json'
+        } else {
+            manifestUrl = 'https://api.artic.edu/api/v1/artworks/' + artworkId + '/manifest.json';
+        }
+        
 
         // Fetch the IIIF manifest
         fetch(manifestUrl)
         .then(response => response.json())
         .then(manifest => {
+            let imageUrl;
+            let tileWidth;
+            let tileHeight;
+
             // Extract the image URL and tile information from the manifest
-            var imageUrl = manifest["sequences"][0]["canvases"][0]["images"][0]["resource"]["@id"];
-            var tileWidth = manifest["sequences"][0]["canvases"][0]["images"][0]["resource"]["width"];
-            var tileHeight = manifest["sequences"][0]["canvases"][0]["images"][0]["resource"]["height"];
+            if (cogappImage) {
+                imageUrl = manifest["@id"] + '/full/full/0/default.jpg';
+                tileWidth = manifest["tiles"][0]["width"];
+                tileHeight = manifest["tiles"][0]["height"];
+            } else {
+                imageUrl = manifest["sequences"][0]["canvases"][0]["images"][0]["resource"]["@id"];
+                tileWidth = manifest["sequences"][0]["canvases"][0]["images"][0]["resource"]["width"];
+                tileHeight = manifest["sequences"][0]["canvases"][0]["images"][0]["resource"]["height"];
+            }
             
             // Construct the OpenSeadragon tileSources object
             var tileSources = [{
@@ -56,95 +74,5 @@ export function OpenSeadragonViewer({artworkId, idPrefix}) {
         ></div>
     );
 }
-// export function OpenSeadragonViewer({artworkId, idPrefix}) {
-//     console.log(artworkId);
-//     console.log(idPrefix);
-
-//     // Construct the manifest url 
-//     let manifestUrl = 'https://api.artic.edu/api/v1/artworks/' + artworkId + '/manifest.json';
-
-//     console.log(manifestUrl);
-
-//     // Fetch the IIIF manifest
-//     fetch(manifestUrl)
-//     .then(response => response.json())
-//     .then(manifest => {
-//         // Extract the image URL and tile information from the manifest
-//         var imageUrl = manifest["sequences"][0]["canvases"][0]["images"][0]["resource"]["@id"];
-//         var tileWidth = manifest["sequences"][0]["canvases"][0]["images"][0]["resource"]["width"];
-//         var tileHeight = manifest["sequences"][0]["canvases"][0]["images"][0]["resource"]["height"];
-        
-//         // Construct the OpenSeadragon tileSources object
-//         var tileSources = [{
-//             type: 'image',
-//             url: imageUrl,
-//             buildPyramid: false,
-//             tileSize: tileWidth,
-//             tileOverlap: 0,
-//             width: tileWidth,
-//             height: tileHeight
-//         }];
-
-//         // Create the OpenSeadragon viewer with the IIIF manifest as the tile source
-//         let viewer = OpenSeadragon({
-//             id: idPrefix,
-//             prefixUrl: '/openseadragon/images/',
-//             crossOriginPolicy: 'Anonymous',
-//             showSequenceControl: false,
-//             showHomeControl: false,
-//             showZoomControl: false,
-//             showFullPageControl: false,
-//             visibilityRatio: 0.3,
-//             homeFillsViewer: false,
-//             autoHideControls: true,
-//             showNavigator: true,
-//             navigatorPosition: 'TOP_LEFT',
-//             navigatorAutoFade: true,
-//             tileSources: tileSources
-//         });
-
-//         console.log(viewer);
-
-//         return viewer;
-//     })
-//     .catch(error => console.error(error));
-
-// }
-
-// const OpenSeadragonViewer = ({ idPrefix, iiifImageId }) => {
-//     const [viewer, setViewer] = useState(null)
-
-//     // Create a new OpenSeadragon viewer
-//     useEffect(() => {
-//         // This should be inside useEffect so it's updated each time the hook runs
-//         const viewerElement = document.getElementById(`${idPrefix}_osd-viewer`);
-
-//         // It's possible that viewer is null but the OpenSeadragon viewer already
-//         // exists in the container div. Do another check for child elements to
-//         // prevent instantiating two viewers
-//         if (
-//             iiifImageId &&
-//             !viewer &&
-//             !(viewerElement && viewerElement.childElementCount > 0)
-//         ) {
-//             setViewer(
-//                 OpenSeadragon({
-//                     id: `${idPrefix}_osd-viewer`,
-//                     tileSources: [iiifImageId + '/info.json'],
-//                     prefixUrl: "//openseadragon.github.io/openseadragon/images/",
-//                 })
-//             )
-//         }
-//     }, [viewer, iiifImageId])
-
-//     return (
-//         !!iiifImageId && (
-//         <div
-//             id={`${idPrefix}_osd-viewer`}
-//             className="w-full h-screen border-4 border-iiif-yellow bg-white"
-//         />
-//         )
-//     )
-// }
 
 export default OpenSeadragonViewer
--- app/getObjectAndImages.js
+++ app/getObjectAndImages.js
@@ -1,14 +1,14 @@
 export default function getObjectAndImages() {
     return new Promise((resolve, reject) => {
         const dibbleObjects = {
-            "Monkey": ["27992", "94126", "26777", "21181", "8715", "14799", "19471", "28341", "52497"],
-            "Dog": ["27992", "212812", "212927", "189201", "40724", "158445", "4594", "61715"],
-            "Boat": ["27992", "14598", "28102", "212909", "30839", "212869", "86421"],
-            "Umbrella": ["27992", "47928", "75097", "19693", "121740", "86421", "198606", "265800"],
-            "Fruit": ["94126", "21181", "81558", "44892", "153703", "64001", "182381"],
-            "Clock": ["34181", "30839", "64383", "156538", "212869", "120116", "58536", "20005", "50297"],
-            "Snake": ["60510", "118661", "142595", "60587", "212812", "212927", "212796", "51466"],
-            "Elephant": ["66103", "6810", "149630", "190847", "66092", "81558"]
+            "Monkey": ["27992", "94126", "26777", "21181", "8715", "14799", "19471", "28341", "52497", "1715937356533card001monkeydogboat", "1715937458290card006monkeyclockelephant", "1715937472118card007monkeyfruitsnake", "1715937356533card001monkeydogboat"],
+            "Dog": ["27992", "212812", "212927", "189201", "40724", "158445", "4594", "61715", "1715937356533card001monkeydogboat", "1715937389260card002dogumbrellafruit", "1715937484999card008dogsnakeelephant", "1715937356533card001monkeydogboat"],
+            "Boat": ["27992", "14598", "28102", "212909", "30839", "212869", "86421", "1715937356533card001monkeydogboat", "1715937389260card002dogumbrellafruit", "1715937413583card003boatclocksnake", "1715937356533card001monkeydogboat"],
+            "Umbrella": ["27992", "47928", "75097", "19693", "121740", "86421", "198606", "265800", "1715937427455card004umbrellasnakeelephant"],
+            "Fruit": ["94126", "21181", "81558", "44892", "153703", "64001", "182381", "1715937389260card002dogumbrellafruit", "1715937443012card005fruitclockelephant", "1715937472118card007monkeyfruitsnake", "1715937356533card001monkeydogboat"],
+            "Clock": ["34181", "30839", "64383", "156538", "212869", "120116", "58536", "20005", "50297", "1715937413583card003boatclocksnake", "1715937443012card005fruitclockelephant", "1715937458290card006monkeyclockelephant"],
+            "Snake": ["60510", "118661", "142595", "60587", "212812", "212927", "212796", "51466", "1715937413583card003boatclocksnake", "1715937427455card004umbrellasnakeelephant", "1715937472118card007monkeyfruitsnake", "1715937484999card008dogsnakeelephant"],
+            "Elephant": ["66103", "6810", "149630", "190847", "66092", "81558", "1715937427455card004umbrellasnakeelephant", "1715937443012card005fruitclockelephant", "1715937458290card006monkeyclockelephant", "1715937484999card008dogsnakeelephant"]
         };
     
         // Get an array of the dibble object names
@@ -20,15 +20,19 @@ export default function getObjectAndImages() {
         // Select two random image IDs from the chosen object array
         const images = dibbleObjects[randomDibbleObject];
         let randomImage1, randomImage2, otherImages;
+        let isPairInOtherArray = [];
     
         do {
-          randomImage1 = images[Math.floor(Math.random() * images.length)];
-          randomImage2 = images[Math.floor(Math.random() * images.length)];
-      
-          // Check if the two image IDs are in another dibbleObject array
-          const otherObjects = dibbleObjectNames.filter(name => name !== randomDibbleObject);
-          otherImages = otherObjects.flatMap(name => dibbleObjects[name]);
-        } while (randomImage1 === randomImage2 || otherImages.includes(randomImage1) || otherImages.includes(randomImage2));
+            randomImage1 = images[Math.floor(Math.random() * images.length)];
+            randomImage2 = images[Math.floor(Math.random() * images.length)];
+          
+            // Check if the two image IDs are in another dibbleObject array together
+            const otherObjects = dibbleObjectNames.filter(name => name !== randomDibbleObject);
+            isPairInOtherArray = otherObjects.some(name => {
+              const otherImages = dibbleObjects[name];
+              return otherImages.includes(randomImage1) && otherImages.includes(randomImage2);
+            });
+          } while (randomImage1 === randomImage2 || isPairInOtherArray);
     
         console.log(randomDibbleObject, randomImage1, randomImage2);
         resolve({
